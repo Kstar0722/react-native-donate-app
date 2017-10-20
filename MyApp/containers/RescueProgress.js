7702f816efde8900f6a59ffa4f3c85a72385c68c
STAR-MAC <star_mac@STAR-MACs-Mac.local>
2017-10-21 07:57:57 +0800

    Update

---
+++ MyApp/containers/RescueProgress.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  Modal,
+  Text
+} from 'react-native'
+import styles from '../Styles/MainScreenStyles'
+import { Images } from '../DevTheme'
+import TapBar from './Tapbar'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon1 from 'react-native-vector-icons/Ionicons'
+import SideMenu from 'react-native-side-menu'
+import SideBar from './SideMenu'
+
+export default class RescueProgress extends Component {
+
+    constructor () {
+        super()
+        
+    }
+
+    render()
+    {
+        
+    }
+}
